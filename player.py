080ef4fcecb915c3941b973d0b2c113a292105cd
Stanislav <sidelnikov.si.001@gmail.com>
2023-11-04 12:28:21 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 class Player:
+    id: int
+    name: str
+    scores: int = 0
+    games: list = []
+
     def __init__(self, id: int, name: str) -> None:
-        self.id: int = id
-        self.name: str = name
-        self.scores: int = 0
-        self.games: list = []
+        self.id = id
+        self.name = name
 
     def add_result(self, game_id: int, scores: int) -> None:
         self.games.append(game_id)
         self.scores += scores
 
     def get_mean(self) -> float:
-        ngames = len(self.games)
+        ngames: int = len(self.games)
         return self.scores / ngames if ngames != 0 else 0
