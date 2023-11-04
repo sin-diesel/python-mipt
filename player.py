fdc59038142e3d4b42584b580e6cb17b680f2cd1
Stanislav <sidelnikov.si.001@gmail.com>
2023-11-04 13:55:00 +0300

    Add task 3 for module 6

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import abc
+
 class Player:
     id: int
     name: str
@@ -17,3 +19,35 @@ class Player:
     def get_mean(self) -> float:
         ngames: int = len(self.games)
         return self.scores / ngames if ngames != 0 else 0
+
+class Power(abc.ABC):
+    name: str
+    cost: int
+
+    def __init__(self, name: str, cost: int) -> None:
+        self.name = name
+        self.cost = cost
+
+    @abc.abstractmethod
+    def use(self, player: Player) -> None:
+        pass
+
+class PhysicalPower(Power):
+    def __init__(self, name: str, cost: int, count: int) -> None:
+        super().__init__(name, cost)
+        self.count = count
+
+    def use(self, player: Player) -> None:
+        if self.count > 0:
+            print(f"{player.name} used {self.name}")
+        else:
+            print(f"{player.name} cannot use {self.name}")
+        self.count -= 1
+
+class MagicPower(Power):
+    def __init__(self, name: str, cost: int) -> None:
+        super().__init__(name, cost)
+
+    def use(self, player: Player) -> None:
+        print(f"{player.name} used {self.name}")
+        player.scores += 1
