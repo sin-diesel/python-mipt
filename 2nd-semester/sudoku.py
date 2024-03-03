6e67ac1bba6d5c93996713ec8c65f6f3f2d9d00d
Stanislav <sidelnikov.si.001@gmail.com>
2024-03-03 20:27:39 +0300

    Add second sudoku task with Command pattern

--- 2nd-semester/sudoku.py
+++ 2nd-semester/sudoku.py
@@ -11,11 +11,11 @@ class Sudoku:
         array: SudokuField,
         constraints: _List[_Callable[[SudokuField], bool]]
     ) -> None:
-        self._array = array
-        self._constraints = constraints
+        self.array = array
+        self.constraints = constraints
 
     def check(self) -> bool:
-        return all([constraint(self._array) for constraint in self._constraints])
+        return all([constraint(self.array) for constraint in self.constraints])
 
 
 def correct_numbers(field: SudokuField) -> bool:
@@ -28,16 +28,29 @@ def correct_dimensions(field: SudokuField) -> bool:
     return len(field) == 9 and all(len(row) == 9 for row in field)
 
 
-s = Sudoku(
-    [[3, 5, 0, 1, 8, 6, 9, 0, 0],
-     [2, 9, 8, 7, 4, 3, 6, 0, 5],
-     [1, 6, 7, 9, 5, 0, 4, 8, 3],
-     [4, 8, 0, 5, 2, 7, 3, 6, 9],
-     [0, 3, 2, 6, 1, 4, 5, 7, 8],
-     [5, 7, 6, 3, 9, 8, 2, 4, 1],
-     [7, 2, 9, 0, 6, 0, 1, 3, 4],
-     [8, 4, 5, 2, 3, 1, 7, 9, 6],
-     [6, 1, 0, 4, 7, 9, 8, 5, 2]],
-    [correct_numbers, correct_dimensions])
+class SudokuOperation:
+    def __init__(self, x: int, y: int, value: int) -> None:
+        self.x = x
+        self.y = y
+        self.value = value
 
-print(s.check())
+
+class SudokuCommand:
+    def __init__(self, sudoku: Sudoku, x: int, y: int, number: int) -> None:
+        self.sudoku = sudoku
+        self.x = x
+        self.y = y
+        self.number = number
+        self.history = list[SudokuOperation]
+
+    def do(self):
+        self.history.append(SudokuOperation(self.x,
+                                            self.y,
+                                            self.sudoku.array[self.x],
+                                            self.sudoku.array[self.y]))
+        self.sudoku.array[self.x][self.y] = self.number
+
+    def undo(self):
+        prev_value = self.history[-1]
+        self.sudoku.array[prev_value.x][prev_value.y] = prev_value.value
+        self.history.pop()
