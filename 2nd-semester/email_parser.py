0de4a581c0a20c00a8d1cc363343dc94add7aae1
Stanislav <sidelnikov.si.001@gmail.com>
2024-04-21 00:15:05 +0300

    Change regex for email parser

--- 2nd-semester/email_parser.py
+++ 2nd-semester/email_parser.py
@@ -4,7 +4,7 @@ import re
 from collections import Counter
 
 def _main() -> None:
-    email_regex = re.compile(r"[\w\.\-]+@[\w\-]+\.\w+")
+    email_regex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 
     filename = "enron_3000.csv"
     with open(filename, "r") as f:
