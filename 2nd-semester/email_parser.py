adb9436bc4a5ea43dee270181e3a0dd73a48bb0d
Stanislav <sidelnikov.si.001@gmail.com>
2024-04-21 00:36:39 +0300

    Switch to parsing by line

--- 2nd-semester/email_parser.py
+++ 2nd-semester/email_parser.py
@@ -4,16 +4,23 @@ import re
 from collections import Counter
 
 def _main() -> None:
-    email_regex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+    email_regex = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
 
     filename = "enron_3000.csv"
     with open(filename, "r") as f:
-        text = f.read()
+        lines = f.readlines()
 
-    matches = re.findall(email_regex, text)
+    matches: list[str] = []
+    for line in lines:
+        cells = line.split(",")
+        for cell in cells:
+            matches.extend(re.findall(email_regex, cell))
     most_common = Counter(matches).most_common(20)
-    for email in most_common:
-        print(f"{email[0]} {email[1]}")
+    for email_freq in most_common:
+        email = email_freq[0]
+        freq = email_freq[1]
+        email = email.strip(".")
+        print(f"{email} {freq}")
 
 
 if __name__ == "__main__":
