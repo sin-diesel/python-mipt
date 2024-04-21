44c7981f714bdce53daff08266409b034db73034
Stanislav <sidelnikov.si.001@gmail.com>
2024-04-21 16:45:41 +0300

    Add armenia.html, add counting number of images

--- 2nd-semester/bs_training.py
+++ 2nd-semester/bs_training.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-fr
+from bs4 import BeautifulSoup
 
 def _main() -> None:
-    pass
+    filename = "armenia.html"
+    with open(filename, "r") as f:
+        html_doc = f.read()
+
+    soup = BeautifulSoup(html_doc, "html.parser")
+    images = soup.find_all("img")
+    nimages = len(images)
+    import code; code.interact(local={**locals(), **globals()})
 
 if __name__ == "__main__":
     _main()
